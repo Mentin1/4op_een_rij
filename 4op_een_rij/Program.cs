b86ebdf83f4fcd68da13020703320375d80adb28
Quintin <Quintin.mens200906@leeuwenborgh.nl>
2017-10-05 14:53:18 +0200

    Add project files.

---
+++ 4op_een_rij/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4op_een_rij
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
